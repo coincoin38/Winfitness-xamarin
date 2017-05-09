05f615a3b32ce15c0046154df6f6bb4504380c8a
gimenez julien <julien.gimenez@nestle.com>
2017-05-09 12:03:42 +0200

    New stubs

--- Winfitness/Pages/CalendarPage.xaml.cs
+++ Winfitness/Pages/CalendarPage.xaml.cs
@@ -13,10 +13,39 @@ namespace Winfitness
 			InitializeComponent();
 			List<Day> days = JSONParser.daysFeed();
 			List<Session> mondaySessions = JSONParser.mondaySessionsFeed();
+            List<Session> tuesdaySessions = JSONParser.tuesdaySessionsFeed();
+			List<Session> wednesdsaySessions = JSONParser.wednesdaySessionsFeed();
+			List<Session> thursdaySessions = JSONParser.thursdaySessionsFeed();
+			List<Session> fridaySessions = JSONParser.fridaydaySessionsFeed();
+			List<Session> satudraySessions = JSONParser.saturdaySessionsFeed();
 
-			foreach (var day in days)
+			foreach (Day day in days)
 			{
-				day.Sessions = mondaySessions;
+				int caseSwitch = Convert.ToInt32(day.Id);
+
+				switch (caseSwitch)
+				{
+					case 1:
+						day.Sessions = mondaySessions;
+						break;
+					case 2:
+						day.Sessions = tuesdaySessions;
+						break;
+					case 3:
+						day.Sessions = wednesdsaySessions;
+						break;
+					case 4:
+						day.Sessions = thursdaySessions;
+						break;
+					case 5:
+						day.Sessions = fridaySessions;
+						break;
+					case 6:
+						day.Sessions = satudraySessions;
+						break;
+					default:
+						break;
+				}
 			}
 
 			DaysListView.ItemsSource = days;
--- Winfitness/Services/JSONParser.cs
+++ Winfitness/Services/JSONParser.cs
@@ -10,46 +10,130 @@ namespace Winfitness
 		public static List<Sport> sportsFeed()
 		{
 			return JsonConvert.DeserializeObject<List<Sport>>(
-				@"[{id:'1',name:'CXWORX',description_id:'1',color:'#f47a1f',image:'cxworx2'},
-				   {id:'2',name:'ZUMBA',description_id:'2',color:'#cc0099',image:'zumba'},
-				   {id:'3',name:'STEP',description_id:'3',color:'#00859c',image:'bodystep'}, 
-				   {id:'4',name:'ATTACK',description_id:'4',color:'#fec424',image:'bodyattack'}, 
-				   {id:'5',name:'BALANCE',description_id:'5',color:'#cadf87',image:'bodybalance'}, 
-				   {id:'6',name:'COMBAT',description_id:'6',color:'#817b00',image:'bodycombat'}, 
-				   {id:'7',name:'PUMP',description_id:'7',color:'#fe0000',image:'bodypump'}, 
-				   {id:'8',name:'WAVE',description_id:'8',color:'#333399',image:'bodyvive'},
-				   {id:'9',name:'RPM',description_id:'9',color:'#00a3e3',image:'rpm-2'}, 
-				   {id:'10',name:'JAM',description_id:'10',color:'#ec9cc3',image:'bodyjam'}]");
+				@"[{ id:'1', name:'CXWORX', description_id:'1', color:'#f47a1f', image:'cxworx2' },
+				   { id:'2', name:'ZUMBA', description_id:'2', color:'#cc0099', image:'zumba' },
+				   { id:'3', name:'STEP', description_id:'3', color:'#00859c', image:'bodystep' }, 
+				   { id:'4', name:'ATTACK', description_id:'4', color:'#fec424', image:'bodyattack' }, 
+				   { id:'5', name:'BALANCE', description_id:'5', color:'#cadf87', image:'bodybalance' }, 
+				   { id:'6', name:'COMBAT', description_id:'6', color:'#817b00', image:'bodycombat' }, 
+				   { id:'7', name:'PUMP', description_id:'7', color:'#fe0000', image:'bodypump' }, 
+				   { id:'8', name:'WAVE', description_id:'8', color:'#333399', image:'bodyvive' },
+				   { id:'9', name:'RPM', description_id:'9', color:'#00a3e3', image:'rpm-2' }, 
+				   { id:'10', name:'JAM', description_id:'10', color:'#ec9cc3', image:'bodyjam' }]");
 		}
 
 		public static List<Day> daysFeed()
 		{
 			return JsonConvert.DeserializeObject<List<Day>>(
-				@"[{id:'1',name:'Lundi'},
-				   {id:'2',name:'Mardi'},
-				   {id:'3',name:'Mercredi'}, 
-				   {id:'4',name:'Jeudi'}, 
-				   {id:'5',name:'Vendredi'}, 
-				   {id:'6',name:'Samedi'}]");
+				@"[{ id:'1', name:'Lundi' },
+				   { id:'2', name:'Mardi' },
+				   { id:'3', name:'Mercredi' }, 
+				   { id:'4', name:'Jeudi' }, 
+				   { id:'5', name:'Vendredi' }, 
+				   { id:'6', name:'Samedi' }]");
 		}
 
 		public static List<Session> mondaySessionsFeed()
 		{
 			return JsonConvert.DeserializeObject<List<Session>>(
-					@"[{sport_id: '3',from: '09:00',duration: '15',day: 'Lundi',attendance: false}, 
-					   {sport_id: '3',from: '09:15',duration: '45',day: 'Lundi',attendance: true}, 
-				       {sport_id: '5',from: '10:00',duration: '30',day: 'Lundi',attendance: true}, 
-					   {sport_id: '6',from: '12:15',duration: '15',day: 'Lundi',attendance: false}, 
-					   {sport_id: '6',from: '12:30',duration: '45',day: 'Lundi',attendance: true}, 
-					   {sport_id: '1',from: '13:15',duration: '30',day: 'Lundi',attendance: true}, 
-					   {sport_id: '8',from: '14:30',duration: '45',day: 'Lundi',attendance: true}, 
-					   {sport_id: '7',from: '17:00',duration: '30',day: 'Lundi',attendance: true}, 
-					   {sport_id: '5',from: '17:30',duration: '30',day: 'Lundi',attendance: true}, 
-					   {sport_id: '1',from: '18:00',duration: '30',day: 'Lundi',attendance: true}, 
-					   {sport_id: '7',from: '18:30',duration: '45',day: 'Lundi',attendance: true}, 
-				       {sport_id: '9',from: '19:30',duration: '45',day: 'Lundi',attendance: true}, 
-					   {sport_id: '2',from: '19:30',duration: '45',day: 'Lundi',attendance: true}, 
-					   {sport_id: '1',from: '20:15',duration: '30',day: 'Lundi',attendance: true}]");
+					@"[{ sport_id: '3', from: '09:00', duration: '15', day: 'Lundi', attendance: false }, 
+					   { sport_id: '3', from: '09:15', duration: '45', day: 'Lundi', attendance: true }, 
+				       { sport_id: '5', from: '10:00', duration: '30', day: 'Lundi', attendance: true }, 
+					   { sport_id: '6', from: '12:15', duration: '15', day: 'Lundi', attendance: false }, 
+					   { sport_id: '6', from: '12:30', duration: '45', day: 'Lundi', attendance: true }, 
+					   { sport_id: '1', from: '13:15', duration: '30', day: 'Lundi', attendance: true }, 
+					   { sport_id: '8', from: '14:30', duration: '45', day: 'Lundi', attendance: true }, 
+					   { sport_id: '7', from: '17:00', duration: '30', day: 'Lundi', attendance: true }, 
+					   { sport_id: '5', from: '17:30', duration: '30', day: 'Lundi', attendance: true }, 
+					   { sport_id: '1', from: '18:00', duration: '30', day: 'Lundi', attendance: true }, 
+					   { sport_id: '7', from: '18:30', duration: '45', day: 'Lundi', attendance: true }, 
+				       { sport_id: '9', from: '19:30', duration: '45', day: 'Lundi', attendance: true }, 
+					   { sport_id: '2', from: '19:30', duration: '45', day: 'Lundi', attendance: true }, 
+					   { sport_id: '1', from: '20:15', duration: '30', day: 'Lundi', attendance: true }]");
+		}
+
+		public static List<Session> tuesdaySessionsFeed()
+		{
+			return JsonConvert.DeserializeObject<List<Session>>(
+					@"[{ sport_id: '5',from: '09:00',duration: '15',day: 'Mardi',attendance: false },  
+				       { sport_id: '5',from: '09:15',duration: '45',day: 'Mardi',attendance: true }, 
+                       { sport_id: '8',from: '10:00',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '7',from: '12:15',duration: '15',day: 'Mardi',attendance: false },
+                       { sport_id: '7',from: '12:30',duration: '45',day: 'Mardi',attendance: true }, 
+                       { sport_id: '2',from: '14:30',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '1',from: '15:15',duration: '30',day: 'Mardi',attendance: true },
+                       { sport_id: '6',from: '16:45',duration: '15',day: 'Mardi',attendance: false },
+                       { sport_id: '6',from: '17:00',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '3',from: '17:45',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '9',from: '18:30',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '4',from: '18:45',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '7',from: '19:30',duration: '45',day: 'Mardi',attendance: true },
+                       { sport_id: '5',from: '20:15',duration: '45',day: 'Mardi',attendance: true }]");
+		}
+
+		public static List<Session> wednesdaySessionsFeed()
+		{
+			return JsonConvert.DeserializeObject<List<Session>>(
+					@"[{ sport_id: '8', from: '09:15', duration: '45', day: 'Mercredi', attendance: true }, 
+					   { sport_id: '2', from: '10:00', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '4', from: '12:15', duration: '15', day: 'Mercredi', attendance: false }, 
+				       { sport_id: '4', from: '12:30', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '1', from: '13:15', duration: '30', day: 'Mercredi', attendance: true },  
+					   { sport_id: '5', from: '14:15', duration: '15', day: 'Mercredi', attendance: false },  
+					   { sport_id: '5', from: '14:30', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '7', from: '16:45', duration: '15', day: 'Mercredi', attendance: false },  
+					   { sport_id: '7', from: '17:00', duration: '15', day: 'Mercredi', attendance: true },  
+					   { sport_id: '4', from: '17:45', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '5', from: '18:30', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '9', from: '19:30', duration: '45', day: 'Mercredi', attendance: true },  
+					   { sport_id: '1', from: '19:30', duration: '30', day: 'Mercredi', attendance: true },  
+					   { sport_id: '6', from: '20:00', duration: '45', day: 'Mercredi', attendance: true }]");
+		}
+
+		public static List<Session> thursdaySessionsFeed()
+		{
+			return JsonConvert.DeserializeObject<List<Session>>(
+					@"[{ sport_id: '7', from: '09:00', duration: '15', day: 'Jeudi', attendance: false },
+					   { sport_id: '7', from: '09:15', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '1', from: '10:00', duration: '30', day: 'Jeudi', attendance: true },
+					   { sport_id: '3', from: '12:15', duration: '15', day: 'Jeudi', attendance: false },
+					   { sport_id: '3', from: '12:30', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '3', from: '14:15', duration: '15', day: 'Jeudi', attendance: false },
+					   { sport_id: '7', from: '14:30', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '8', from: '17:15', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '1', from: '18:00', duration: '30', day: 'Jeudi', attendance: true },
+					   { sport_id: '9', from: '18:30', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '2', from: '18:30', duration: '45', day: 'Jeudi', attendance: true }, 
+					   { sport_id: '4', from: '19:30', duration: '45', day: 'Jeudi', attendance: true },
+					   { sport_id: '7', from: '20:15', duration: '45', day: 'Jeudi', attendance: true }]");
+		}
+		public static List<Session> fridaydaySessionsFeed()
+		{
+			return JsonConvert.DeserializeObject<List<Session>>(
+					@"[{ sport_id: '1', from: '09:15', duration: '30', day: 'Vendredi', attendance: true },
+					   { sport_id: '8', from: '09:45', duration: '45', day: 'Vendredi', attendance: true },
+                       { sport_id: '1', from: '12:30', duration: '30', day: 'Vendredi', attendance: true }, 
+				       { sport_id: '5', from: '13:00', duration: '45', day: 'Vendredi', attendance: true },
+                       { sport_id: '7', from: '14:15', duration: '15', day: 'Vendredi', attendance: false },
+                       { sport_id: '3', from: '14:30', duration: '45', day: 'Vendredi', attendance: true }, 
+                       { sport_id: '4', from: '16:45', duration: '15', day: 'Vendredi', attendance: false },
+					   { sport_id: '4', from: '17:00', duration: '30', day: 'Vendredi', attendance: true },
+                       { sport_id: '7', from: '17:30', duration: '45', day: 'Vendredi', attendance: true },
+                       { sport_id: '9', from: '17:30', duration: '45', day: 'Vendredi', attendance: true }, 
+                       { sport_id: '3', from: '18:30', duration: '45', day: 'Vendredi', attendance: true },
+                       { sport_id: '10', from: '19:30', duration: '45', day: 'Vendredi', attendance: true },
+                       { sport_id: '5', from: '20:15', duration: '45', day: 'Vendredi', attendance: true }]");
+		}
+
+		public static List<Session> saturdaySessionsFeed()
+		{
+			return JsonConvert.DeserializeObject<List<Session>>(
+					 @"[{ sport_id: '5', from: '09:00', duration: '15', day: 'Samedi', attendance: false },
+				        { sport_id: '5', from: '09:15', duration: '45', day: 'Samedi', attendance: true },
+                        { sport_id: '4', from: '10:00', duration: '45', day: 'Samedi', attendance: true },
+                        { sport_id: '2', from: '10:45', duration: '45', day: 'Samedi', attendance: true },
+                        { sport_id: '7', from: '11:30', duration: '45', day: 'Samedi', attendance: true },
+                        { sport_id: '9', from: '12:30', duration: '45', day: 'Samedi', attendance: true }]");
 		}
 	}
 }
